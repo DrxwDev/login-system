01f3212d89c4f418f102eca9fc7f114fcf64a07d
DrxwDev <drxwdev@outlook.es>
2026-07-07 17:23:03 -0400

    add: user domain entity and constructor

---
+++ internal/core/domain/user/password.go
@@ -0,0 +1,23 @@
+package user
+
+import "strings"
+
+type Password string
+
+func NewPassword(value string) (Password, error) {
+	value = strings.TrimSpace(value)
+
+	if value == "" {
+		return "", ErrPasswordRequired
+	}
+
+	if len(value) < 8 {
+		return "", ErrPasswordTooShort
+	}
+
+	if len(value) > 255 {
+		return "", ErrPasswordTooLong
+	}
+
+	return Password(value), nil
+}
---
+++ internal/core/domain/user/user_id.go
@@ -0,0 +1,15 @@
+package user
+
+import "strings"
+
+type UserID string
+
+func NewUserID(value string) (UserID, error) {
+	value = strings.TrimSpace(value)
+
+	if value == "" {
+		return "", ErrUserIDRequired
+	}
+
+	return UserID(value), nil
+}
