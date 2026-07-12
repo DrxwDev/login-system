05397cf16434050d6f5968f3423693220c7082b8
DrxwDev <drxwdev@outlook.es>
2026-07-12 14:09:12 -0400

    fix: user registration to return a token, with user data

--- internal/core/usecase/user/register.go
+++ internal/core/usecase/user/register.go
@@ -12,45 +12,52 @@ import (
 type RegisterUseCase struct {
 	repo   ports.UserRepository
 	hasher ports.PasswordHasher
+	auth   ports.TokenManager
 }
 
-func NewRegisterUseCase(repo ports.UserRepository, hasher ports.PasswordHasher) *RegisterUseCase {
+func NewRegisterUseCase(repo ports.UserRepository, hasher ports.PasswordHasher, auth ports.TokenManager) *RegisterUseCase {
 	return &RegisterUseCase{
 		repo:   repo,
 		hasher: hasher,
+		auth:   auth,
 	}
 }
 
-func (uc *RegisterUseCase) Register(ctx context.Context, params RegisterParams) (user.User, error) {
+func (uc *RegisterUseCase) Register(ctx context.Context, params RegisterParams) (user.User, string, error) {
 	id, err := user.NewUserID(uuid.NewString())
 	if err != nil {
-		return user.User{}, err
+		return user.User{}, "", err
 	}
 
 	email, err := user.NewEmail(params.Email)
 	if err != nil {
-		return user.User{}, err
+		return user.User{}, "", err
 	}
 
 	hash, err := uc.hasher.Hash(params.Password)
 	if err != nil {
-		return user.User{}, err
+		return user.User{}, "", err
 	}
 
 	password, err := user.NewHashedPassword(hash)
 	if err != nil {
-		return user.User{}, err
+		return user.User{}, "", err
 	}
 
 	newUser, err := user.NewUser(id, params.Name, email, password)
 	if err != nil {
-		return user.User{}, err
+		return user.User{}, "", err
+	}
+
+	token, err := uc.auth.Generate(newUser.ID.String())
+	if err != nil {
+		return user.User{}, "", err
 	}
 
 	err = uc.repo.Create(ctx, newUser)
 	if err != nil {
-		return user.User{}, err
+		return user.User{}, "", err
 	}
 
-	return newUser, nil
+	return newUser, token, nil
 }
